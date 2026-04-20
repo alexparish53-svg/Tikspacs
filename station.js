110ab9ea6735a67bd630c0adc5e118cbac55a54c
alexparish53-svg <alexparish53@gmail.com>
2026-04-20 15:20:06 +0200

    Add files via upload

---
+++ station.js
@@ -0,0 +1,11 @@
+var stationer = [
+  { namn: "Höganäs Hamn", lat: 56.1981, lon: 12.5613, status: "aktiv" },
+  { namn: "Lerberget", lat: 56.1604, lon: 12.5721, status: "inaktiv" },
+  { namn: "Viken", lat: 56.1402, lon: 12.5939, status: "aktiv" },
+  { namn: "Nyhamnsläge", lat: 56.2399, lon: 12.5422, status: "aktiv" },
+  { namn: "Höganäs Centrum", lat: 56.1988, lon: 12.5759, status: "inaktiv" },
+  { namn: "Jonstorp", lat: 56.2260, lon: 12.6740, status: "aktiv" },
+  { namn: "Mölle", lat: 56.2846, lon: 12.5015, status: "inaktiv" },
+  { namn: "Arild", lat: 56.2680, lon: 12.5730, status: "aktiv" },
+  { namn: "Kullens Fyr", lat: 56.3000, lon: 12.4500, status: "aktiv" }
+];
